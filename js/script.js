aeb92060b6c4c6729890f625e221c481d61abd35
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-06-28 16:34:58 -0700

    Add styling to styles.css and create an auto-generated list of plants and their indexes that displays when the button is clicked in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
+const houseplants = [
+  "Pothos",
+  "Philodendron",
+  "Monstera Deliciosa",
+  "Bird of Paradise",
+  "Peace Lily",
+  "ZZ plant",
+  "Bromeliad",
+  "Fiddle Leaf Fig",
+  "Anthurium",
+  "Money tree",
+  "Dracaena",
+  "Rubber plant",
+  "Spider plant",
+  "Cast Iron plant",
+  "Snake plant",
+  "Lucky Bamboo",
+  "Aloe Vera",
+];
 
+const plantList = document.querySelector(".list");
+const button = document.querySelector(".reveal-list");
+const totalCount = document.querySelector(".total-count");
+
+button.addEventListener("click", function () {
+  plantList.classList.remove("hide");
+  button.classList.add("hide");
+  plantNames(houseplants);
+});
+
+totalCount.innerText = `${houseplants.length} of our favorite houseplants!`;
+
+const plantNames = function (list) {
+  // access the value & index position of each plant
+  list.forEach(function (plant, index) {
+    let li = document.createElement("li");
+    li.classList.add("plant");
+    li.innerHTML = `<span class="plant-number">#${index + 1}</span> ${plant}`;
+    plantList.append(li);
+  });
+};
